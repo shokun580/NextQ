ac82d6669a8e01fe6f166a793f4f610049098a1e
Aphinan <154401387+shokun580@users.noreply.github.com>
2025-09-16 10:36:53 +0700

    chore: comment out unused Link import from react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const navigation = [
   { name: 'Product', href: '#' },
